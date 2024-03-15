391efcf88692d143522bc33862eed4603b871997
rodentskie <rodentskie@gmail.com>
2024-03-15 16:40:16 +0800

    feat: additional events

--- library/go/dynamo-db/main.go
+++ library/go/dynamo-db/main.go
@@ -52,73 +52,6 @@ func InsertItem(svc *dynamodb.DynamoDB, item *types.TablePullRequestData) error 
 	return nil
 }
 
-func GetSlackTimeStampReviewRequest(svc *dynamodb.DynamoDB, item *types.ReviewRequestPullRequest) (string, error) {
-	tableName := env.GetEnv("TABLE_NAME", "PullRequests")
-
-	result, err := svc.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"id": {
-				S: aws.String(strconv.Itoa(int(item.PullRequest.ID))),
-			},
-			"pullRequestId": {
-				N: aws.String(strconv.Itoa(int(item.Number))),
-			},
-		},
-	})
-
-	if err != nil {
-		return "", err
-	}
-	if result.Item == nil {
-		msg := "Could not find '" + strconv.Itoa(int(item.Number)) + "'"
-		return "", errors.New(msg)
-	}
-
-	var data types.TablePullRequestData
-	err = dynamodbattribute.UnmarshalMap(result.Item, &data)
-	if err != nil {
-		return "", err
-	}
-	return data.SlackTimeStamp, nil
-}
-
-func GetSlackTimeStampIssue(svc *dynamodb.DynamoDB, item *types.CommentPullRequest) (string, error) {
-	tableName := env.GetEnv("TABLE_NAME", "PullRequests")
-
-	result, err := svc.Query(&dynamodb.QueryInput{
-		TableName: aws.String(tableName),
-		IndexName: aws.String("PullRequestIdIndex"),
-		KeyConditions: map[string]*dynamodb.Condition{
-			"pullRequestId": {
-				ComparisonOperator: aws.String("EQ"),
-				AttributeValueList: []*dynamodb.AttributeValue{
-					{
-						N: aws.String(strconv.Itoa(item.Issue.Number)),
-					},
-				},
-			},
-		},
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	var data []types.TablePullRequestData
-	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &data)
-	if err != nil {
-		return "", err
-	}
-
-	var timeStamp string
-	for _, item := range data {
-		timeStamp = item.SlackTimeStamp
-	}
-
-	return timeStamp, nil
-}
-
 func GetSlackTimeStampClose(svc *dynamodb.DynamoDB, item *types.ClosedPullRequest) (string, error) {
 	tableName := env.GetEnv("TABLE_NAME", "PullRequests")
 
@@ -148,4 +81,40 @@ func GetSlackTimeStampClose(svc *dynamodb.DynamoDB, item *types.ClosedPullReques
 		return "", err
 	}
 	return data.SlackTimeStamp, nil
+}
+
+func GetSlackTimeStamp(svc *dynamodb.DynamoDB, pullRequestId int) (string, error) {
+	tableName := env.GetEnv("TABLE_NAME", "PullRequests")
+
+	result, err := svc.Query(&dynamodb.QueryInput{
+		TableName: aws.String(tableName),
+		IndexName: aws.String("PullRequestIdIndex"),
+		KeyConditions: map[string]*dynamodb.Condition{
+			"pullRequestId": {
+				ComparisonOperator: aws.String("EQ"),
+				AttributeValueList: []*dynamodb.AttributeValue{
+					{
+						N: aws.String(strconv.Itoa(pullRequestId)),
+					},
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	var data []types.TablePullRequestData
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &data)
+	if err != nil {
+		return "", err
+	}
+
+	var timeStamp string
+	for _, item := range data {
+		timeStamp = item.SlackTimeStamp
+	}
+
+	return timeStamp, nil
 }
--- library/go/slack/main.go
+++ library/go/slack/main.go
@@ -2,9 +2,7 @@ package slack
 
 import (
 	"fmt"
-	"slack-pr-lambda/constants"
 	"slack-pr-lambda/env"
-	"slack-pr-lambda/mapstruct"
 	"slack-pr-lambda/types"
 
 	"github.com/slack-go/slack"
@@ -28,30 +26,6 @@ func SlackSendMessage(input types.OpenPullRequest) (string, error) {
 	}
 
 	return timestamp, nil
-}
-
-func SlackSendMessageThreadReviewers(timeStamp string, reviewers []string) error {
-	token := env.GetEnv("SLACK_TOKEN", "")
-	channel := env.GetEnv("SLACK_CHANNEL", "")
-	api := slack.New(token)
-
-	slackUsers := constants.SlackUsers()
-	slackUsersMap := mapstruct.StructToMapInterface(*slackUsers)
-
-	var slackMention string = "Please review: "
-	for _, user := range reviewers {
-		slackMention += fmt.Sprintf("<@%s>", slackUsersMap[user])
-	}
-
-	_, _, err := api.PostMessage(
-		channel,
-		slack.MsgOptionText(slackMention, false),
-		slack.MsgOptionTS(timeStamp),
-	)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func SlackSendMessageThreadClosed(timeStamp string) error {
@@ -87,103 +61,3 @@ func SlackSendMessageThread(timeStamp string, message string) error {
 	}
 	return nil
 }
-
-/*
-
-package handlers
-
-import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"slack-pr-lambda/env"
-	"slack-pr-lambda/logger"
-	"syscall"
-
-	"github.com/slack-go/slack"
-	"go.uber.org/zap"
-)
-
-func PullRequestHandler(w http.ResponseWriter, r *http.Request) {
-	l := logger.LoggerConfig()
-	zapLog, _ := l.Build()
-	defer func() {
-		err := r.Body.Close()
-		if err != nil {
-			zapLog.Fatal("error close req body",
-				zap.Error(err),
-			)
-		}
-
-		if err := zapLog.Sync(); err != nil && !errors.Is(err, syscall.EINVAL) {
-			log.Fatalf("error closing the logger. %v\n", err)
-		}
-	}()
-	token := env.GetEnv("SLACK_TOKEN", "")
-	api := slack.New(token)
-	inlineCode := "code"
-	codeBlock := `func main() {
-    fmt.Println("Hello, world!")
-}`
-
-	// Use fmt.Sprintf to construct the message text with dynamic content
-	messageText := fmt.Sprintf(`Here are some bullet points:
-• Item 1
-• Item 2
-• Item 3
-
-And here is an inline code: `+"`%s`"+`
-
-And a code block:
-`+"```%s```"+`
-
-`, inlineCode, codeBlock)
-
-	channelID, timestamp, err := api.PostMessage(
-		"pull-requests",                         // Channel name. Ensure your bot is a member of this channel.
-		slack.MsgOptionText(messageText, false), // Passing the message with bullet points
-		slack.MsgOptionAsUser(true),             // Send as a user, not as a bot
-	)
-	if err != nil {
-		log.Fatalf("Failed to send message: %v", err)
-	}
-
-	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
-
-	threadTimestamp := "1710406869.549219"
-	_, _, err = api.PostMessage(
-		channelID,
-		slack.MsgOptionText("This is a reply in a thread! v2", false),
-		slack.MsgOptionTS(threadTimestamp),
-	)
-	if err != nil {
-		log.Fatalf("Failed to send message: %v", err)
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		zapLog.Fatal("error read request body",
-			zap.Error(err),
-		)
-	}
-
-	bodyBytes := Response{
-		Message: string(body),
-	}
-
-	j, err := json.Marshal(bodyBytes)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(j)
-
-}
-
-
-*/
