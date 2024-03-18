6f374b47033536287f826237d9570eb7e03b459e
rodentskie <rodentskie@gmail.com>
2024-03-18 11:45:21 +0800

    fix: test and lint for constants lib

---
+++ library/go/constants/emoji_test.go
@@ -0,0 +1,29 @@
+package constants
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestEmoji(t *testing.T) {
+	expected := &Emojis{
+		CheckPassed:      ":check-passed:",
+		CheckFailed:      ":check-failed:",
+		Approved:         ":approved:",
+		Closed:           ":closed:",
+		Merged:           ":merged:",
+		Opened:           ":opened:",
+		PullRequest:      ":pull-request:",
+		Pushed:           ":pushed:",
+		RequestedChanges: ":requested-changes:",
+		Reviewed:         ":reviewed:",
+		RequestReview:    ":eyes:",
+		Comment:          ":writing_hand:",
+	}
+
+	result := Emoji()
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("struct does not match the expected.")
+	}
+}
---
+++ library/go/constants/users_test.go
@@ -0,0 +1,26 @@
+package constants
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSlackUsers(t *testing.T) {
+	expected := &Users{
+		PaulWaltersDev:     "U020E8T5PC5",
+		MeganSitoyPractera: "U04MFRK6350",
+		Rodentskie:         "U046JKYN3BQ",
+		Trtshen:            "U1GBY5XKJ",
+		TerenceCoder:       "U1GJ48N5V",
+		Jazzmind:           "U02TB2WV7",
+		Shawnm0705:         "U0DFSQLJC",
+		Sasangachathumal:   "U9JD7Q9GF",
+		Sunilsbcloud:       "U016R04BE81",
+	}
+
+	result := SlackUsers()
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("struct does not match the expected.")
+	}
+}
