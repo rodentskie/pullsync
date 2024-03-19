1588adfa8053afe2e595ce0612c206995dc9884c
rodentskie <rodentskie@gmail.com>
2024-03-20 06:58:42 +0800

    fix: nx script affected

--- tools/scripts/print-affected.sh
+++ tools/scripts/print-affected.sh
@@ -5,6 +5,6 @@ set -e
 COMMIT_RANGE="origin/$BASE_REF"
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-PROJECT=$(./node_modules/.bin/nx show projects --affected --base="$COMMIT_RANGE" --head="$CURRENT_BRANCH" --select=projects)
+PROJECT=$(pnpm nx show projects --affected --base="$COMMIT_RANGE" --head="$CURRENT_BRANCH" --select=projects)
 
 echo "$PROJECT"
