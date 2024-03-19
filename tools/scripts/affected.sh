08538c30b8b70032f864a126ff16bcc0db78ca02
rodentskie <rodentskie@gmail.com>
2024-03-20 06:45:38 +0800

    chore: idk

--- tools/scripts/affected.sh
+++ tools/scripts/affected.sh
@@ -6,4 +6,4 @@ COMMIT_RANGE="origin/$BASE_REF"
 AFFECTED="${TARGET:-apps}"
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-./node_modules/.bin/nx affected -t "$AFFECTED" --base="$COMMIT_RANGE" --head="$CURRENT_BRANCH" --parallel=6 --output-style=stream
+./node_modules/.bin/nx affected -t "$AFFECTED" --base="$COMMIT_RANGE" --head="$CURRENT_BRANCH" --parallel=6
