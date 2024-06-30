23c7e20e9ace92a306f32c040a3aa1fa500af86e
rodentskie <rodentskie@gmail.com>
2024-07-01 05:44:10 +0800

    fix: test

--- library/go/constants/users_test.go
+++ library/go/constants/users_test.go
@@ -9,13 +9,14 @@ func TestSlackUsers(t *testing.T) {
 	expected := &Users{
 		PaulWaltersDev:     "U020E8T5PC5",
 		MeganSitoyPractera: "U04MFRK6350",
-		Rodentskie:         "U046JKYN3BQ",
+		Rodentskie:         "U06Q5GKADME",
 		Trtshen:            "U1GBY5XKJ",
 		TerenceCoder:       "U1GJ48N5V",
 		Jazzmind:           "U02TB2WV7",
 		Shawnm0705:         "U0DFSQLJC",
 		Sasangachathumal:   "U9JD7Q9GF",
 		Sunilsbcloud:       "U016R04BE81",
+		Rodentskiie:        "U06Q7E7QFNX",
 	}
 
 	result := SlackUsers()
